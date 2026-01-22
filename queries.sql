53eb30337d6fcf973f62ff8a2c17d7d2dfab2fc3
shantunu007 <chakmashantunu.web@gmail.com>
2026-01-22 20:56:59 +0600

    added vehicles table with constraints

--- queries.sql
+++ queries.sql
@@ -7,3 +7,16 @@ create table
         password varchar(150) not null,
         phone_number varchar(20) not null
     );
+
+create table
+    if not exists vehicles (
+        vehicle_id serial primary key,
+        vehicle_name varchar(150) not null,
+        type varchar(50) not null check (type in ('car', 'bike', 'truck')),
+        model varchar(50) not null,
+        registration_number varchar(150) unique not null,
+        rental_price_per_day decimal(10, 2) not null,
+        availability_status varchar(20) not null check (
+            availability_status in ('available', 'rented', 'maintenance')
+        )
+    );
